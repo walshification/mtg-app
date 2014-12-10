9022c6781028f008cc1abe3db07e9d900567a45e
walshification <walshification@gmail.com>
2014-12-09 21:32:07 -0600

    battlefield styling and beginning shuffle method

--- app/assets/javascripts/decks_ctrl.js
+++ app/assets/javascripts/decks_ctrl.js
@@ -42,6 +42,24 @@
       }
     }
 
+    $scope.shuffle = function(deck) {
+      var m = deck.length, t, i;
+
+      // While there remain elements to shuffle…
+      while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = deck[m];
+        deck[m] = deck[i];
+        deck[i] = t;
+      }
+
+      return deck;
+    }
+
     window.scope = $scope;
 
   });
