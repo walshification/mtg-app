16e0ba9c858bcbec7745a3f53f58869135b4b617
Christopher Walsh <walshification@gmail.com>
2016-04-30 14:52:04 -0500

    adds annotations to models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: cards
+#
+#  id            :integer          not null, primary key
+#  multiverse_id :integer
+#  deck_id       :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#  card_name     :string(255)
+#  image_url     :string(255)
+#  card_type     :string(255)
+#  card_subtype  :string(255)
+#
+
 class Card < ActiveRecord::Base
   validates :deck_id, :presence => true
   
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: decks
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  name         :string(255)
+#  legal_format :string(255)
+#  deck_type    :string(255)
+#  color        :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Deck < ActiveRecord::Base
   validates :name, :presence => true
   validates :name, :uniqueness => true
