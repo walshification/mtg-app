eab2cc4a95903f555d1960fba00d39cc1b60578c
Christopher Walsh <walshification@gmail.com>
2016-05-01 11:30:40 -0500

    defines custom rspec matcher for pg constraint

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+require 'support/matchers/violate_check_constraint_matcher'
 
 describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "email" do
+    let(:user) {
+      User.create!(
+        email: "foo@example.com",
+        password: "qwertyuiop",
+        password_confirmation: "qwertyuiop",
+      )
+    }
+
+    it "absolutely prevents invalid email addresses" do
+      expect {
+        user.update_attribute(:email, "foo@_.com")
+      }.to violate_check_constraint(:email_must_be_valid_email)
+    end
+  end
 end
