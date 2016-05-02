29bf3d8dd4664a42ce161f4ac59292c4b0848bb5
Christopher Walsh <walshification@gmail.com>
2016-05-01 21:32:19 -0500

    creates a dumb test, gets tests passing

--- spec/models/card_scraper_spec.rb
+++ spec/models/card_scraper_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CardScraper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be true" do
+    expect(true).to be_truthy
+  end
 end
