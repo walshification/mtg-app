6b5ea245b0df0e801696fb6b373aa0dca8dec777
Christopher Walsh <walshification@gmail.com>
2016-05-02 22:02:39 -0500

    add csrf token for api auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
+  after_filter :set_csrf_cookie_for_ng
+
+  protected
+
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
+
+  private
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 end
