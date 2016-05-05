da38359d0197fe2602a6d4fc269850e775beab09
Christopher Walsh <walshification@gmail.com>
2016-05-05 01:11:24 -0500

    adds rudimentary angular card search

--- app/assets/javascripts/decks_ctrl.js
+++ app/assets/javascripts/decks_ctrl.js
@@ -5,15 +5,40 @@
 
     $scope.init = function(user_id, deck_id) {
       $scope.userId = user_id;
+      $scope.artifacts = [];
+      $scope.conspiracies = [];
+      $scope.creatures = [];
+      $scope.enchantments = [];
+      $scope.instants = [];
+      $scope.lands = [];
+      $scope.phenomenons = [];
+      $scope.planes = [];
+      $scope.planeswalkers = [];
+      $scope.schemes = [];
+      $scope.sorceries = [];
+      $scope.tribals = [];
+      $scope.vanguards = [];
+      $scope.cardGroups = {
+        "Artifact": $scope.artifacts,
+        "Conspiracy": $scope.conspiracies,
+        "Creature": $scope.creatures,
+        "Enchantment": $scope.enchantments,
+        "Instant": $scope.instants,
+        "Land": $scope.lands,
+        "Phenomenon": $scope.phenomenons,
+        "Plane": $scope.planes,
+        "Planeswalker": $scope.planeswalkers,
+        "Scheme": $scope.schemes,
+        "Sorcery": $scope.sorceries,
+        "Tribal": $scope.tribals,
+        "Vanguard": $scope.vanguards,
+      };
+
       $http.get("/api/v1/decks/" + deck_id + ".json").then(function (response) {
         $scope.deck = response.data;
-        $scope.sortByType($scope.deck["cards"]);
+        $scope.sortCardsByType($scope.deck["cards"]);
       });
     };
-
-    $scope.search = function(searchTerm) {
-      $scope.searchedFor = searchTerm;
-    }
 
     $scope.createDeck = function (newDeckName, newDeckColors,
                                   newDeckType, newDeckFormat) {
@@ -36,49 +61,26 @@
       $scope.newDeckFormat = "";
     };
 
-    $scope.cardGroups = {
-      "Artifacts": [],
-      "Creatures": [],
-      "Enchantments": [],
-      "Instants": [],
-      "Lands": [],
-      "Planeswalkers": [],
-      "Sorceries": []
+    $scope.search = function(searchTerm) {
+      $http.get("/api/v1/cards.json", { "params": {"card_name": searchTerm } }
+        ).then(function (response) {
+          $scope.sortCardByType(response.data);
+        }, function (error) {
+          $scope.error = error.statusText;
+        });
+
+      $scope.searchTerm = "";
     }
 
-    $scope.sortByType = function (cards) {
+    $scope.sortCardByType = function(card) {
+      $scope.cardGroups[card.card_type].push(card);
+    }
+
+    $scope.sortCardsByType = function(cards) {
       for (var i = 0; i < cards.length; i++) {
-        switch (cards[i].card_type) {
-        case "Artifact":
-          $scope.cardGroups["Artifacts"].push(cards[i]);
-          break;
-        case "Creature":
-        case "Artifact Creature":
-        case "Legendary Creature":
-        case "Legendary Artifact Creature":
-          $scope.cardGroups["Creatures"].push(cards[i]);
-          break;
-        case "Enchantment":
-          $scope.cardGroups["Enchantments"].push(cards[i]);
-          break;
-        case "Instant":
-          $scope.cardGroups["Instants"].push(cards[i]);
-          break;
-        case "Basic Land":
-        case "Land":
-          $scope.cardGroups["Lands"].push(cards[i]);
-          break;
-        case "Planeswalker":
-          $scope.cardGroups["Planeswalkers"].push(cards[i]);
-          break;
-        case "Sorcery":
-          $scope.cardGroups["Sorceries"].push(cards[i]);
-          break;
-        }
+        $scope.sortCardByType(cards[i]);
       }
     }
-
-    $scope.cardImageInGallery;
 
     $scope.galleryCard = function(cardImage) {
       $scope.cardImageInGallery = cardImage;
--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::CardsController < ApplicationController
   def index
+    @card = Card.where("name LIKE ?", params[:card_name]).first
+    puts @card
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @card }
+    end
   end
 
   def show
@@ -13,10 +19,10 @@ class Api::V1::CardsController < ApplicationController
     @deck = Deck.find_by(:id => params[:deck_id])
     lookup_card = TolarianRegistry::Card.find_by_name(params[:card_name])
     @card = Card.new({
-      :multiverse_id => lookup_card.multiverse_id, 
-      :deck_id => params[:deck_id], 
-      :card_name => lookup_card.card_name, 
-      :image_url => lookup_card.image_url, 
+      :multiverse_id => lookup_card.multiverse_id,
+      :deck_id => params[:deck_id],
+      :card_name => lookup_card.card_name,
+      :image_url => lookup_card.image_url,
       :card_type => lookup_card.card_type,
       :card_subtype => lookup_card.card_subtype
     })
