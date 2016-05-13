30df9cdaf2505ad1eaec7c5a748648f2caeac077
Christopher Walsh <walshification@gmail.com>
2016-05-12 23:32:57 -0500

    intro to httpBackend

--- spec/javascripts/app/controllers/cards_controller_spec.js
+++ spec/javascripts/app/controllers/cards_controller_spec.js
@@ -1,8 +1,7 @@
 describe("cardsCtrl", function() {
 
   describe("initialization", function() {
-
-    var scope = null,
+    var scope      = null,
         controller = null;
 
     beforeEach(module("app"));
@@ -18,5 +17,46 @@ describe("cardsCtrl", function() {
       expect(scope.cards).toEqualData([]);
     });
 
+  });
+
+  describe("Getting Search Results", function() {
+    var scope       = null,
+        controller  = null,
+        httpBackend = null,
+        serverResults = [
+          {
+            id: 1,
+            multiverse_id: 102,
+            name: "Counterspell",
+          },
+          {
+            id: 2,
+            multiverse_id: 103,
+            name: "Fireball",
+          },
+        ];
+
+    beforeEach(module("app"));
+
+    beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+      scope       = $rootScope.$new();
+      httpBackend = $httpBackend;
+      controller  = $controller("cardsCtrl", {
+        $scope: scope
+      });
+    }));
+
+
+    beforeEach(function() {
+      httpBackend.when('GET',
+                       '/api/v1/cards.json?name=Counterspell&page=0').
+                  respond(serverResults);
+    })
+
+    it("populates the card list with the results", function() {
+      scope.search("Counterspell");
+      httpBackend.flush();
+      expect(scope.cards).toEqualData(serverResults);
+    });
   });
 });
