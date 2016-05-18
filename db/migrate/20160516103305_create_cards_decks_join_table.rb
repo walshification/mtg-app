b1b05bdd52dbb2cdc7f8829e7db1a2dbb94e39a3
Christopher Walsh <walshification@gmail.com>
2016-05-18 07:30:58 -0500

    removes migrations that dont need to exist

--- db/migrate/20160516103305_create_cards_decks_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateCardsDecksJoinTable < ActiveRecord::Migration
-  def change
-    create_table :cards_decks, id: false do |t|
-      t.belongs_to :deck, index: true
-      t.belongs_to :card, index: true
-    end
-  end
-end
--- db/migrate/20160516124912_drop_cards_decks_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCardsDecksTable < ActiveRecord::Migration
-  def change
-    drop_table :cards_decks
-  end
-end
