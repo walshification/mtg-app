560a8b33011510c7c159d75947ce172f7678daac
Christopher Walsh <walshification@gmail.com>
2017-09-04 16:47:55 -0500

    refactors tests with fixtures

--- spec/models/gatherer_spec.rb
+++ spec/models/gatherer_spec.rb
@@ -3,144 +3,22 @@
 require 'rails_helper'
 
 describe Gatherer, type: :model do
-  let(:set_response) { double('set response') }
-  let(:tms_response) { double('test magic set response') }
-  let(:ats_response) { double('another test set response') }
-  let(:test_card_response) { double('test card response') }
-  let(:test_sets) do
-    {
-      'sets' => [
-        {
-          'code' => 'TMS',
-          'name' => 'Test Magic Set',
-          'type' => 'expansion',
-          'border' => 'black',
-          'booster' => [
-            [
-              'rare',
-              'mythic rare'
-            ],
-            'uncommon',
-            'uncommon',
-            'uncommon',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'land',
-            'marketing'
-          ],
-          'releaseDate' => '2017-11-03',
-          'magicCardsInfoCode' => 'tms',
-          'block' => 'Test Magic Block'
-        },
-        {
-          'code' => 'ATS',
-          'name' => 'Another Test Set',
-          'type' => 'expansion',
-          'border' => 'white',
-          'booster' => [
-            [
-              'rare'
-            ],
-            'uncommon',
-            'uncommon',
-            'uncommon',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'common',
-            'land',
-            'marketing'
-          ],
-          'releaseDate' => '2017-12-12',
-          'magicCardsInfoCode' => 'ats',
-          'block' => 'Test Magic Block'
-        }
-      ]
-    }
-  end
-  let(:test_cards) do
-    {
-      'cards' => [
-        {
-          'name' => 'some card',
-          'manaCost' => '{1}{W}',
-          'cmc' => 2,
-          'colors' => ['White'],
-          'colorIdentity' => ['W'],
-          'type' => 'Creature — Angel',
-          'types' => ['Creature'],
-          'subtypes' => ['Angel'],
-          'rarity' => 'Mythic Rare',
-          'set' => 'RTR',
-          'setName' => 'Test Magic Set',
-          'text' => 'Flying',
-          'artist' => 'Chris Walsh',
-          'number' => '1',
-          'power' => '5',
-          'toughness' => '6',
-          'flavor' => 'That good, good flavor.',
-          'layout' => 'normal',
-          'multiverseid' => 8675309,
-          'imageUrl' => 'imageurl.com/assets/card.jpg',
-          'rulings' => [
-            {
-              'date' => '2017-12-01',
-              'text' => 'This is the best card ever.'
-            }
-          ],
-          'foreignNames' => [
-            {
-              'name' => 'Some Foreign Name',
-              'imageUrl' => 'imageurl.com/assets/card.jpg',
-              'language' => 'Some Foreign Language',
-              'multiverseid' => 5556703
-            }
-          ],
-          'printings' => %w[RTR C15],
-          'originalText' => 'Original text',
-          'originalType' => 'Creature — Angel',
-          'legalities' => [
-            {
-              'format' => 'Commander',
-              'legality' => 'Legal'
-            }
-          ],
-          'id' => '32e9a9ca8a42301c73857bb6f1cb1eb81bec59f6'
-        }
-      ]
-    }
-  end
+  let(:test_response) { double('test card response') }
+  let(:api_response) { YAML.load_file('spec/fixtures/magic_api_responses.yml') }
   let(:no_cards) { { 'cards' => [] } }
 
   subject { Gatherer.new.gather }
 
   before(:each) do
-    allow(set_response).to receive(:parsed_response) { test_sets }
-    allow(HTTParty).to receive(:get).with(/.*sets/) { set_response }
-    allow(tms_response).to receive(:parsed_response).and_return(
-      test_cards,
+    allow(test_response).to receive(:parsed_response).and_return(
+      api_response, # once for sets
+      api_response, # next five for cards
       no_cards,
       no_cards,
       no_cards,
       no_cards
     )
-    allow(HTTParty).to receive(:get).with(/.*cards\?set=TMS&page=[\d]+/) { tms_response }
-    allow(ats_response).to receive(:parsed_response) { { 'cards' => [] } }
-    allow(HTTParty).to receive(:get).with(/.*cards\?set=ATS&page=[\d]+/) { ats_response }
+    allow(HTTParty).to receive(:get) { test_response }
   end
 
   describe '#gather' do
@@ -197,6 +75,12 @@ describe Gatherer, type: :model do
         expect(retrieved_card.original_text).to eq(nil)
         expect(retrieved_card.original_type).to eq(nil)
         expect(retrieved_card.source).to eq(nil)
+      end
+
+      it 'only saves cards once' do
+        subject
+        subject
+        expect(Card.count).to eq(1)
       end
     end
   end
